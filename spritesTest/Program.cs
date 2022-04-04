29b1b6a2e2f76a3afd4dfd8f9ef3c017b5e435ff
Henry Eriksson <eriksson.henry@elev.ga.ntig.se>
2022-04-04 14:32:37 +0200

    bfea

--- spritesTest/Program.cs
+++ spritesTest/Program.cs
@@ -7,14 +7,14 @@ namespace spritesTest
     {
         static void Main(string[] args)
         {
-            Texture2D sheet = Raylib.LoadTexture(@"mort.png");
-
-            Sprite player = new Sprite(sheet, 100, 100);
 
 
 
             Raylib.InitWindow(1920 / 2, 1080 / 2, "spritesTest");
-            Raylib.SetTargetFPS(5);
+            Raylib.SetTargetFPS(10);
+
+            Texture2D poo = Raylib.LoadTexture(@"mort.png");
+            Sprite player = new Sprite(poo, 200, 100);
 
             while (Raylib.WindowShouldClose() == false)
             {
@@ -22,10 +22,34 @@ namespace spritesTest
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.BLACK);
 
-                player.Idle();
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT) && !Raylib.IsKeyDown(KeyboardKey.KEY_LEFT) || !Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT) && Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT_SHIFT))
+                    {
+                        player.Run();
+                    }
+                    else
+                    {
+                        player.Walk();
+                    }
+                }
+                else if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT) && Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    player.Stare();
+                }
+                else
+                {
+                    player.Idle();
+                }
+
+
+
 
                 Raylib.EndDrawing();
             }
         }
+
+
     }
 }
--- spritesTest/Sprite.cs
+++ spritesTest/Sprite.cs
@@ -1,5 +1,7 @@
 using Raylib_cs;
 using System.Numerics;
+using System.Timers;
+using System.Threading;
 
 namespace spritesTest
 {
@@ -8,9 +10,9 @@ namespace spritesTest
         Texture2D pic;
         int x;
         int y;
-        int startFrame;
-        int endFrame;
         int onFrame;
+        int f = 1;
+
 
         public Sprite(Texture2D _pic, int _x, int _y)
         {
@@ -19,39 +21,61 @@ namespace spritesTest
             y = _y;
         }
 
-        public void DisplayAnimFrame(Texture2D pic, int dx, int dy, int spriteID, int w = 24, int h = 24)
+        public void DisplayAnimFrame(Texture2D _pic, int dx, int dy, int spriteID, int w = 24, int h = 24)
         {
+            if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) f = -1;
+            else if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) f = 1;
+
+
             int x = (spriteID % w) * w;
             int y = (spriteID / w) * w;
-            Rectangle source = new Rectangle(x, y, w, h);
-            Rectangle dest = new Rectangle(dx, dy, w, h);
+            Rectangle source = new Rectangle(x, y, w * f, h);
+            Rectangle dest = new Rectangle(dx, dy, w * 20, h * 20);
             Raylib.DrawTexturePro(pic, source, dest, Vector2.Zero, 0.0f, Color.WHITE);
         }
 
         public void AnimPlay(int startFrame, int endFrame)
         {
-            onFrame = startFrame;
-
-            if (onFrame < endFrame)
-            {
-                onFrame++;
-            }
-            else
+            if (startFrame == endFrame)
             {
                 onFrame = startFrame;
+                System.Console.WriteLine(onFrame);
+            }
+            else if (onFrame < startFrame || onFrame > endFrame)
+            {
+                onFrame = startFrame;
+                System.Console.WriteLine(onFrame);
+            }
+            else if (onFrame < endFrame && onFrame > startFrame || onFrame == startFrame)
+            {
+                onFrame++;
+                System.Console.WriteLine(onFrame);
+            }
+            else if (onFrame == endFrame)
+            {
+                onFrame = startFrame;
+                System.Console.WriteLine(onFrame);
             }
             DisplayAnimFrame(pic, x, y, onFrame);
         }
+
 
         public void Idle()
         {
             AnimPlay(0, 3);
         }
-        public void Run()
+        public void Walk()
         {
-            AnimPlay(4, 10);
+            AnimPlay(4, 9);
         }
 
-
+        public void Run()
+        {
+            AnimPlay(18, 23);
+        }
+        public void Stare()
+        {
+            AnimPlay(24, 24);
+        }
     }
 }
